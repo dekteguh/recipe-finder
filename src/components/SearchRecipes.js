658234f89b249508034a0f21c23f7d65eef49bd9
Eko Teguh Widodo <dekteguh@gmail.com>
2017-08-16 03:13:17 +0700

    Fetching from API

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -2,22 +2,73 @@ import React, { Component } from 'react';
 import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            ingredients: '',
+            dish: ''
+        }
+
+        this.handleChangeIngredients = this.handleChangeIngredients.bind(this);
+        this.handleChangeDish = this.handleChangeDish.bind(this);
+        this.handleClickSubmit = this.handleClickSubmit.bind(this);
+    }
+
+    handleChangeIngredients(e) {
+        this.setState({
+            ingredients: e.target.value
+        });
+        //console.log('state', this.state);
+    }
+
+    handleChangeDish(e) {
+        this.setState({
+            dish: e.target.value
+        });
+        //console.log('state', this.state);
+    }
+
+    handleClickSubmit(e) {
+        let { ingredients, dish } = this.state;
+        const URL = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+        //console.log('state', this.state, 'url', URL);
+
+        fetch(URL, {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((json) => console.log('recipes', json))
+            .catch((err) => console.log('error', err));
+    }
+
     render() {
         return (
             <Form inline>
                 <FormGroup>
                     <ControlLabel>Ingredients</ControlLabel>
                     {' '}
-                    <FormControl type="text" placeholder="garlic, chicken"/>
+                    <FormControl
+                        type="text"
+                        placeholder="garlic, chicken"
+                        onChange={this.handleChangeIngredients}
+                    />
                 </FormGroup>
                 {' '}
                 <FormGroup>
                     <ControlLabel>Dish</ControlLabel>
                     {' '}
-                    <FormControl type="text" placeholder="adobo" />
+                    <FormControl
+                        type="text"
+                        placeholder="adobo"
+                        onChange={this.handleChangeDish}
+                    />
                 </FormGroup>
                 {' '}
-                <Button className="btn btn-success">Submit</Button>
+                <Button
+                    className="btn btn-success"
+                    onClick={this.handleClickSubmit}
+                >Submit</Button>
             </Form>
         )
     }
