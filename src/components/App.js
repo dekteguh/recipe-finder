90330c72af042848be088d5ef741a06104402a81
Eko Teguh Widodo <dekteguh@gmail.com>
2017-08-15 20:42:44 +0700

    Initialize commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Recipe Finder</h1>
+        )
+    }
+}
+
+export default App;
